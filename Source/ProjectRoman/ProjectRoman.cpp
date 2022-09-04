69d1ab4cd7a273df002e13ed862624b354ea5a00
Danny-sth <danny.wasth@gmail.com>
2022-09-05 01:43:00 +0300

    First Commit

---
+++ Source/ProjectRoman/ProjectRoman.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectRoman.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectRoman, "ProjectRoman" );
